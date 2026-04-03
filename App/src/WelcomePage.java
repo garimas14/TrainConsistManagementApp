02bc0fa2087166b0d45145dcd2a6c01b69797eea
garimas14 <gs2432@srmist.edu.in>
2026-04-03 22:36:50 +0530

    GARIMAS ADDED UC1

---
+++ App/src/WelcomePage.java
@@ -0,0 +1,22 @@
+/**
+ * Book My Stay App
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * This class represents the entry point of the Hotel Booking Management System.
+ * It demonstrates how a Java application starts execution and displays output.
+ *
+ * @author Garima
+ * @version 1.0
+ */
+
+public class WelcomePage {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("welcome to the hotel booking management system");
+
+        // System initialization message
+        System.out.println("system initialized successfully");
+    }
+}
